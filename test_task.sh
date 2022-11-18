afda6e49c2469e99b147974f149494131a930c65
kamil383 <kamil.safin.96@mail.ru>
2022-11-19 02:43:15 +0400

    first commit

---
+++ test_task.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+server_name=("$@")
+now=$(date +"%d_%m_%Y")
+if [ -z "$server_name" ]
+
+then
+    server_name="SERVER_${now}_running"
+else
+    server_name+="_${now}_running"
+fi
+
+curl -s  https://raw.githubusercontent.com/GreatMedivack/files/master/list.out | awk '{if ($3=="Running") print $1}' > $server_name.out
+
+
+arc_name=$(echo $server_name | grep  -Po '(.*\d)')
+
+
+
+if [ -f "archives/$arc_name.tar" ]
+then
+    echo "Архив уже создан"
+else
+     tar -cf  archives/$arc_name.tar $server_name.out
+fi
+
+
+
+
+
